fe5e00035b30a38d6891004a3085a0a38e36fcb1
BAIIF <86160635+BAIIF@users.noreply.github.com>
2021-09-11 19:08:58 +0200

    Add files via upload

--- server.py
+++ server.py
@@ -20,23 +20,39 @@ def process_image():
         file = request.files['image']
         img = Image.open(file.stream)
         img = img.save("saved.jpg")
+
+    if os.stat("saved.jpg").st_size > 1000000:
+        file_size = os.stat("saved.jpg").st_size  # determine the size of th image in bytes
+        quality = 60
+        while file_size > 1000000:  # while the image size more then 1 MB
+            img = Image.open("saved.jpg")  # open the old image
+            img.save("saved.jpg", optimize=True, quality=quality)  # save the new low quality image
+            file_size = os.stat("saved.jpg").st_size  # determine the new file size
+            quality -= 10  # try with less quality
+
+    client = vision.ImageAnnotatorClient()
+    with io.open("saved.jpg", 'rb') as image_file:
+       content = image_file.read()
+    image = vision.Image(content=content)
         
+    response = client.text_detection(image=image)
+    text = response.text_annotations[0].description
 
-        client = vision.ImageAnnotatorClient()
-        with io.open("saved.jpg", 'rb') as image_file:
-           content = image_file.read()
-        image = vision.Image(content=content)
-        
-        response = client.text_detection(image=image)
-        text = response.text_annotations[0].description
+    dates = datefinder.find_dates(text)
+    for n in dates: 
+         dates= str(n) 
 
-        dates = datefinder.find_dates(text)
-        for data in dates: 
-             date= str(data) 
-        result.update({ "date": date  })
-
-    
+    date=""
+    time=""
+    for n in range(0, 10):
+        date+=str(dates[n])
    
+
+    for n in range(11,19):
+        time+=str(dates[n])
+
+
+    result.update({ "date": date, "time":time  })
 
         
       
